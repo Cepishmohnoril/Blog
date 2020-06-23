f695e195328cfb89afb9cd6fb7edf8ac7fe2ff1d
Vladyslav Radchuk <cepishmohnoril@gmail.com>
2020-06-23 22:42:48 +0300

    react-admin in progress

---
+++ admin-front/src/app.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { Admin, Resource } from "react-admin";
+import { ArticleList } from "./view-formatters/articles";
+import blogAdminProvider from './blog-admin-provider';
+
+const dataProvider = blogAdminProvider('http://api.blog.local');
+const App = () => (
+    <Admin dataProvider={dataProvider}>
+        <Resource name="articles" list={ArticleList} />
+    </Admin>
+);
+
+export default App;
